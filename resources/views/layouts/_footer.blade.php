5e2252863289b1345226e6524de3f17ce96386ea
sunjun <827640651@qq.com>
2020-08-01 23:31:58 +0800

    first commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="page-top" title="返回顶部">
+	<a href="#page-top"><span class="iconfont icon-top"></span></a>
+</div>
+<footer class="footer mt-auto py-3 bg-light shadow">
+	<div class="container d-flex flex-row">
+		</div>
+		<div class="text-secondary flex-grow-2">
+			@if (blog_setting('icp'))
+				<a href="http://www.beian.miit.gov.cn" target="_blank">
+					&copy; <span class="small">{{ blog_setting('icp') }}</span>
+				</a>
+			@endif
+		</div>
+	</div>
+</footer>
